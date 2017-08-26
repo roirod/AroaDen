488e55b54a5c2d63c28afe8c1b8d3ad5b56b45b2
roirod <eusonroi@gmail.com>
2017-08-26 19:01:26 +0200

    commit merge develop

--- resources/views/set/index.blade.php
+++ resources/views/set/index.blade.php
@@ -7,12 +7,16 @@
 
 @if( $username == 'admin' )
 
-	<div class="row">  <div class="col-sm-12 mar10">
-	 <div class="input-group"> <span class="input-group-btn pad4"> <p> Usuarios: </p> </span>	
-	 <div class="btn-toolbar pad4" role="toolbar">
-	 <div class="btn-group"> 
-	 	<a href="/Usuarios" role="button" class="btn btn-sm btn-primary"> <i class="fa fa-bars"></i> Ver</a>
-	 </div> 
+	<div class="row"> 
+	  <div class="col-sm-12"> 
+	    <div class="input-group"> 
+	      <span class="input-group-btn pad10">  <p> Usuarios: </p> </span>
+	      <div class="btn-toolbar pad4" role="toolbar"> 
+	        <div class="btn-group">
+	          <a href="{{ url("/Usuarios") }}" role="button" class="btn btn-sm btn-primary">
+	            <i class="fa fa-bars"></i> Ver
+	          </a>
+	        </div>  
 	</div> </div> </div> </div>
 
 @else
