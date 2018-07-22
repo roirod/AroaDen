c7281a19d3b9d9d1bbab88277e70e9866262700d
roirod <eusonroi@gmail.com>
2018-07-22 17:11:38 +0200

    laravel 5.3 upgrade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::singularResourceParameters(false);
     }
 
     /**
